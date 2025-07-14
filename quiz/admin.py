bb3afdef0c6926f8a4990b90a65d51772c68a1d4
YusufSyam <muhyusufsyamsyam@gmail.com>
2025-07-15 00:33:27 +0800

    feat: add list display on admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import UserProfile, Quiz, QuizSession
 
-admin.site.register(UserProfile)
-admin.site.register(Quiz)
-admin.site.register(QuizSession)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'number', 'school', 'tutor_name', 'tutor_number')
+    search_fields = ('name', 'school', 'tutor_name')
+    list_filter = ('school',)
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'bidang', 'start_date', 'end_date')
+    list_filter = ('bidang',)
+    search_fields = ('title',)
+
+@admin.register(QuizSession)
+class QuizSessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'quiz', 'score', 'duration', 'user_start', 'user_end')
+    list_filter = ('quiz__bidang',)
+    search_fields = ('user__username',)
